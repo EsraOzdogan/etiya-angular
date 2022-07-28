117b219c6399c939415f4c8642528750ad0ef3c7
EsraOzdogan <esraozdogaan@gmail.com>
2022-07-28 23:57:41 +0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddProductComponent } from './pages/add-product/add-product.component';
+
+const routes: Routes = [
+  {path:"", redirectTo: 'homepage', pathMatch:"full"},  /*direkt homepage pathini calistiriyor*/
+  {path:"homepage", component:HomepageComponent},  /*pathMatch-- kosul degistiren*/
+  {path:"add-product", component: AddProductComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {      //build edilirken kullanılır
+  production: true,
+  apiUrl : ''
+  // apiUrl : 'http://kodlama.io:3000'
+};
